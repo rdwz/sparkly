b51f411227abe59d80d8e92f8c8df9f87d4512eb
Giacomo Rebonato <grebonato@outlook.ie>
2022-09-07 11:59:55 +0100

    minor ui adjustments

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,14 +8,22 @@ export const Home = () => {
 
   return (
     <main className='container mx-auto grid grid-cols-1 gap-2 pr-1 pl-1'>
+      <section className='grid grid-cols-1 p-4 gap-2'>
+        <h1 className='text-xl text-center'>Fastify-React-TS</h1>
+        <div className='text-center'>
+          <Link href='https://fastify-react-ts.fly.dev' target='_blank'>
+            GitHub
+          </Link>
+        </div>
+      </section>
       <Navbar>
         <Navbar.Start>
           <LoginForm />
         </Navbar.Start>
-        <Navbar.Center>
-          <h1 className='text-center text-2xl'>Fastify - React</h1>
+        <Navbar.Center className='portrait:hidden'>
+          <h1 className='text-center text-xl'>Fastify-React-TS</h1>
         </Navbar.Center>
-        <Navbar.End>
+        <Navbar.End className='portrait:hidden'>
           <Link href='https://fastify-react-ts.fly.dev' target='_blank'>
             GitHub
           </Link>
