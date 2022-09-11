a68929e8469d7e1c7d31569cc31d7a4e01de3f71
Giacomo Rebonato <grebonato@outlook.ie>
2022-09-11 09:34:54 +0100

    More env var for personal data

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    minify: false
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [
+      react(),
+      createHtmlPlugin({
+        minify: true,
+        inject: {
+          data: {
+            SITE_NAME: env.VITE_SITE_NAME,
+            GOOGLE_VERIFICATION_ID: env.VITE_GOOGLE_VERIFICATION_ID,
+          },
+        },
+      }),
+    ],
+    build: {
+      minify: false,
+    },
   }
 })
-
