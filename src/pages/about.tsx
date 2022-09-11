b19c4f20c8bd47c3560b283af902515f0ab39efe
Giacomo Rebonato <grebonato@outlook.ie>
2022-09-11 09:41:23 +0100

    Put out <LoginForm /> by mistake. Put it back.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,13 @@
-import {} from 'react-daisyui'
 import { Layout } from '../components/layout'
 
 export const About = () => {
   return (
     <Layout>
       <h1>About</h1>
+      <p>
+        This is a starter kit for creating a full stack Fastify + React
+        application.
+      </p>
     </Layout>
   )
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,12 @@
 import { Layout } from '../components/layout'
+import { LoginForm } from '../components/login-form'
 import { TaskForm } from '../components/task-form'
 import { TasksTable } from '../components/tasks-table'
 
 export const Home = () => {
   return (
     <Layout>
+      <LoginForm />
       <TaskForm />
       <TasksTable />
     </Layout>
